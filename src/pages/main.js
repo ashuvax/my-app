ace24fdfd38dc6e21a8f7b849fabc40dbcc8326f
ashuvax <ash6783397@gmail.com>
2022-04-07 10:47:22 +0300

    Updates

--- src/pages/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import Joks from "../components/joksFunc";
-import Todos from "../components/Todos";
-
-const main = () => {
-  return <Todos />;
-};
-export default main;
